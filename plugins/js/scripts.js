c1c643d70d4ccef98bed0dd905f5a46d31107cd0
siteations <studer@siteations.com>
2018-10-30 23:13:38 -0500

    secondary page-sample css done

--- plugins/js/scripts.js
+++ plugins/js/scripts.js
@@ -27,6 +27,27 @@ document.addEventListener('DOMContentLoaded', function(){
 	    });
 	});
 
+	//cards interactions - biography style for leadership page
+	$('.bio').each(function(){
+			$(this).click(function(){
+
+				//image toggle
+				($(this).find('img').css('opacity')!=='1')? $(this).find('img').css('opacity', '1') : $(this).find('img').css("opacity", '.1');
+
+				//text position toggle
+				($(this).find('.text').css("top")==='-220px')? $(this).find('.text').css("top", '0px').css("margin-bottom", '0px') : $(this).find('.text').css("top", '-220px').css("margin-bottom", '-220px');
+
+
+				//text toggle
+				var textWithin =[].slice.call($(this).find('p')).filter(p=>p.attributes.data);
+				var text = (textWithin.length===1)? textWithin[0].attributes.data.value: 'sorry, no longer entry' ;
+				var textOld = textWithin[0].innerText;
+
+				$(this).find('p')[1].remove();
+				$(this).find('.text').append(`<p data="${textOld}">${text}</p>`);
+			})
+	})
+
 
 
 });
